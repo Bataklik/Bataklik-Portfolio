5c60cbbcf089b285ee0f85ae00e0e6451edd19c9
Bataklik <60544395+Bataklik@users.noreply.github.com>
2022-09-18 20:56:35 +0200

    feat: :recycle: Small refactor

---
+++ src/components/projectItem/ProjectItem.tsx
@@ -0,0 +1,95 @@
+import { Box, styled } from "@mui/material";
+import { motion } from "framer-motion";
+import React from "react";
+
+interface ProjectItemProps {
+  name: string;
+  description: string;
+  link: string;
+  image: string;
+}
+export default function ProjectItem({
+  name,
+  description,
+  link,
+  image,
+}: ProjectItemProps) {
+  return (
+    <ProjectsItem key={name}>
+      <ProjectInnerBox>
+        <Box>
+          <ProjectsItemTitle>{name}</ProjectsItemTitle>
+          <ProjectsItemDescription>{description}</ProjectsItemDescription>
+        </Box>
+        <a href={link} target="_blank" rel="noreferrer">
+          <ProjectsItemVisitButton>VISIT</ProjectsItemVisitButton>
+        </a>
+      </ProjectInnerBox>
+      <ProjectsItemImage src={image} />
+    </ProjectsItem>
+  );
+}
+const ProjectInnerBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "space-between",
+}));
+
+const ProjectsItemVisitButton = styled(motion.button)(({ theme }) => ({
+  color: "#1B1D22",
+  fontSize: "14px",
+  fontFamily: "inter",
+  fontWeight: 700,
+  textDecoration: "none",
+  marginLeft: "auto",
+  marginTop: "10px",
+  borderRadius: "20px",
+  padding: "15px 20px",
+  backgroundColor: "#FFFFFF",
+  textAlign: "center",
+  width: "100px",
+  height: "50px",
+  cursor: "pointer",
+  "&:hover": {
+    backgroundColor: "#1B1D22",
+    color: "#fff",
+    border: "1px solid #fff",
+  },
+  [theme.breakpoints.down("sm")]: {
+    padding: "10px 15px",
+    width: "90px",
+    height: "40px",
+    borderRadius: "15px",
+  },
+}));
+const ProjectsItem = styled(motion.div)(({ theme }) => ({}));
+const ProjectsItemImage = styled(motion.img)(({ theme }) => ({
+  width: "100%",
+  borderRadius: "20px",
+  boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
+  [theme.breakpoints.down("sm")]: {
+    width: "100%",
+    borderRadius: "20px",
+    boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
+  },
+}));
+const ProjectsItemTitle = styled(motion.h4)(({ theme }) => ({
+  color: "#fff",
+  fontSize: "20px",
+  fontFamily: "inter",
+  fontWeight: 700,
+  marginBottom: "1px",
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "18px",
+  },
+}));
+const ProjectsItemDescription = styled(motion.p)(({ theme }) => ({
+  color: "#fff",
+  fontSize: "16px",
+  fontFamily: "inter",
+  fontWeight: 400,
+  marginTop: "1px",
+  marginBottom: "10px",
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "14px",
+  },
+}));
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,6 +1,7 @@
 import { Box, Container, styled } from "@mui/material";
 import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
+import ProjectItem from "../projectItem/ProjectItem";
 import blobDcaptures from "./../../assets/images/projects/dcapture.png";
 import blobFadedphotography from "./../../assets/images/projects/fadedphotography.png";
 import blobSmartgrid from "./../../assets/images/projects/smartgrid.png";
@@ -44,20 +45,7 @@ export default function Projects() {
         </ProjectsTitle>
         <ProjectsBox>
           {projectsItems.map(item => (
-            <ProjectsItem key={item.name}>
-              <ProjectInnerBox>
-                <Box>
-                  <ProjectsItemTitle>{item.name}</ProjectsItemTitle>
-                  <ProjectsItemDescription>
-                    {item.description}
-                  </ProjectsItemDescription>
-                </Box>
-                <ProjectsVisitButton href={item.link} target="_blank">
-                  VISIT
-                </ProjectsVisitButton>
-              </ProjectInnerBox>
-              <ProjectsItemImage src={item.image} />
-            </ProjectsItem>
+            <ProjectItem {...item} />
           ))}
         </ProjectsBox>
       </ProjectsContainer>
@@ -92,63 +80,5 @@ const ProjectsBox = styled(Box)(({ theme }) => ({
 
   [theme.breakpoints.down("sm")]: {
     padding: "20px 25px",
-  },
-}));
-const ProjectInnerBox = styled(Box)(({ theme }) => ({
-  display: "flex",
-}));
-const ProjectsVisitButton = styled(motion.a)(({ theme }) => ({
-  color: "#1B1D22",
-  fontSize: "14px",
-  fontFamily: "inter",
-  fontWeight: 700,
-  textDecoration: "none",
-  marginLeft: "auto",
-  marginTop: "10px",
-  borderRadius: "20px",
-  padding: "15px 20px",
-  backgroundColor: "#FFFFFF",
-  textAlign: "center",
-  width: "100px",
-  height: "50px",
-  "&:hover": {
-    backgroundColor: "#1B1D22",
-    color: "#fff",
-    border: "1px solid #fff",
-  },
-  [theme.breakpoints.down("sm")]: {
-    padding: "15px 20px",
-  },
-}));
-const ProjectsItem = styled(motion.div)(({ theme }) => ({}));
-const ProjectsItemImage = styled(motion.img)(({ theme }) => ({
-  width: "100%",
-  borderRadius: "20px",
-  boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
-  [theme.breakpoints.down("sm")]: {
-    width: "100%",
-    borderRadius: "20px",
-    boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
-  },
-}));
-const ProjectsItemTitle = styled(motion.h4)(({ theme }) => ({
-  color: "#fff",
-  fontSize: "20px",
-  fontFamily: "inter",
-  fontWeight: 700,
-  marginBottom: "1px",
-  [theme.breakpoints.down("sm")]: {
-    fontSize: "18px",
-  },
-}));
-const ProjectsItemDescription = styled(motion.p)(({ theme }) => ({
-  color: "#fff",
-  fontSize: "16px",
-  fontFamily: "inter",
-  fontWeight: 400,
-  marginTop: "1px",
-  marginBottom: "10px",
-  [theme.breakpoints.down("sm")]: {
-    fontSize: "14px",
   },
 }));
