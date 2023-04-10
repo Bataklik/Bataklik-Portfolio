07eede04622f6a16bce6eb793aafb676df2e3ddb
Bataklik <60544395+Bataklik@users.noreply.github.com>
2023-04-10 15:43:23 +0200

    feat: :white_check_mark: Add

--- next.config.js
+++ next.config.js
@@ -1,8 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const { join } = require("path");
+
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-  jsconfigPaths: true,
-};
+  webpack: (config, { isServer }) => {
+    // Resolve TypeScript path aliases
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@components": join(__dirname, "src/components"),
+      "@utils": join(__dirname, "src/utils"),
+      // add more custom paths as needed
+    };
 
-module.exports = nextConfig;
+    if (!isServer) {
+      // Fix CSS imports in development
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        os: false,
+      };
+    }
+
+    return config;
+  },
+};
