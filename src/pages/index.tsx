d49bdf236103be753336e8f0e7df58d4601b8921
Bataklik <60544395+Bataklik@users.noreply.github.com>
2023-04-10 16:00:39 +0200

    feat: :truck: Rename index -> layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { ContactSection } from "@components/core//contact";
 import { IntroSection } from "@components/core/intro";
 import { ProjectsSection } from "@components/core/project";
 import { SkillSection } from "@components/core/skill";
-import { Layout } from "@components/layout/index";
+import { Layout } from "@components/layout/layout";
 
 export default function Index() {
   return (
